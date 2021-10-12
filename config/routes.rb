afa1f453eb09eb30c523b5fafdbf7e8694ab012e
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-10-11 18:59:37 -0600

    Init devise, add views, config, add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  
   root to: 'application#home'
+
+  Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'omni_auth'
+  }
+  devise_scope :user do
+    get 'sign_in', to: 'users/sessions#new'
+    get 'sign_up', to: 'users/registrations#new'
+    get 'forgot_password', to: 'users/passwords#new'
+    get 'reset_password', to: 'users/passwords#edit'
+  end
+  
+
 end
