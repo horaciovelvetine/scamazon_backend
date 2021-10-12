557a8294387f96f07b361e49448be0467af7e4ea
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-10-11 23:24:34 -0600

    remove additional route drawer

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   
   root to: 'application#home'
 
-  Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
