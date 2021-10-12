6c9c70fe2cc69e1b9f866da366c01c9384f6ab07
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-10-12 16:04:49 -0600

    add in scaff for seed, migrate db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,11 @@ class ApplicationController < ActionController::Base
   # Homepage
   def home; end
 
+  def seed
+
+  end
+  
+
   protected
 
   def configure_permitted_parameters
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_12_193112) do
+ActiveRecord::Schema.define(version: 2021_10_12_220258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -130,6 +130,11 @@ ActiveRecord::Schema.define(version: 2021_10_12_193112) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_searches_on_user_id"
+  end
+
+  create_table "seeds", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "shopping_carts", force: :cascade do |t|
