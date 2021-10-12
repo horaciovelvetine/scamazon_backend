6d41dbde69b65a8d96a72da3b1c2c6974d9bce37
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-10-12 16:04:13 -0600

    add seed route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   root to: 'application#home'
 
+  get '/seed', to: 'application#seed', as: 'seedythedeebee'
+
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
