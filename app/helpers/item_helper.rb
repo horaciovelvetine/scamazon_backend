c214d74071dc30c4f253b92e1220009c1c4e1405
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-10-14 16:25:25 -0600

    add limited get item helper for views

--- app/helpers/item_helper.rb
+++ app/helpers/item_helper.rb
@@ -3,5 +3,13 @@ module ItemHelper
   def get_random_items
     @items = Item.find(Item.distinct.pluck(:id)).sample(20)
   end
+
+  def get_random_item
+    @items = get_random_items
+
+
+    @item = @items.sample(1)
+    # if @@item.exists?
+  end
   
 end
