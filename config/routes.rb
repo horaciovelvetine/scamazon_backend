ecd4317190fb24465737ca934e1a5ae1b0679e1f
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-10-14 14:59:14 -0600

    scope and plan out routes for every resource

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
     reviews: 'users/reviews'
   }
   devise_scope :user do
+
+    ## Add in all of the default routes commonly used to do the same thing
     get 'sign_in', to: 'users/sessions#new'
     get 'sign_up', to: 'users/registrations#new'
     get 'sign_out', to: 'users/sessions#destroy'
@@ -31,12 +33,45 @@ Rails.application.routes.draw do
     get 'reset_password', to: 'users/passwords#edit'
     get 'show_profile', to: 'users/sessions#show'
     get 'profile', to: 'users/sessions#show'
-    resources :gift_cards
-    # resources :orders
-    # resources :lists
-    # resources :shooping_carts
-    # resources :reviews
+    
+
+    ## MIRROR DEVISE USERS ROUTING AND CONTROLLER PATHING
+    scope module: 'users', path: 'users', as: 'users' do 
+    
+      ## RESOURCE: gift_card
+
+      get 'gift_card_manger', to: 'gift_cards#show'
+      post 'gift_card_manger', to: 'gift_cards#create'
+    
+      ## RESOURCE: list && order
+      resources :lists, :order 
+
+      
+      
+    end
+
+    ## NEST REVIEW UNDER ITEM / STILL NEED USER ACCESS
+    scope module: 'items', path: 'item', as: 'item' do
+    
+      ## RESOURCE: review
+      resources :reviews, shallow: true
+
+    end
+
+    ## RESOURCE: shopping_cart
+    #   not nested path because carts do not always belong to a user yet ~~~~>>
+    
+    resources :shooping_carts, only: [:show, :edit, :update, :destroy]
+    
   end
   
+
+  scope module: 'items' do 
+    resources :items 
+  end
+
+  scope module: 'stores' do 
+    resources :inventory, only: [:show, :edit, :update, :destroy]
+  end
 
 end
