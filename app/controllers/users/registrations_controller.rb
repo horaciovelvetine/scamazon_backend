2a4c94e165883b557945a9e3437a53b8796d479e
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-10-14 15:59:47 -0600

    permit add'l sanitized params

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,24 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
+  
   layout false
   
   def after_sign_up_path_for(_resource)
     binding.pry
     profile_path
   end
+
+  protected
+
+  
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone, :address, :slime, :balance ])
+  end
+
+  
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone, :address, :slime, :balance ])
+  end
+
 end
