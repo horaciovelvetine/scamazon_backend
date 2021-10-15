13d214cf43057d86642e673634b6d34f68236e3a
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-10-15 17:45:38 -0600

    add simple info grabbers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+
+
+    def user_first_name
+      @user.first_name
+    end
+
+    def user_last_name
+      @user.last_name
+    end
+
+    def user_phone
+      @user.phone
+    end
+
+    def user_address
+      @user.address
+    end
+
+    def user_email
+      @user.email
+    end
+    
+
+    
 end
