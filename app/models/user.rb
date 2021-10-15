0d99097fd57c4613e4028569f4e332c6040e0de7
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-10-15 15:07:19 -0600

    messing with attribute assigning

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@ class User < ApplicationRecord
 
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|      
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
