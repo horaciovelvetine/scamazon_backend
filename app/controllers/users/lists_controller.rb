77f09c1281695f05c35af1aec99a621989e06d08
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-10-16 12:54:17 -0600

    index redirect added

--- app/controllers/users/lists_controller.rb
+++ app/controllers/users/lists_controller.rb
@@ -1,7 +1,16 @@
 class Users::ListsController < ApplicationController
   
   ## Should redirect to show with a random list chosen
-  def index; end
+  def index
+    @list = @user.lists.first
+    
+    if @list.nil?
+      redirect_to new_user_list_path, notice: 'Start here by makeing your first list!'
+    else
+      redirect_to user_list_path(id: @list.id)
+    end
+  
+  end
 
   ## NO VIEW creates a new list
   def create; end
@@ -10,7 +19,9 @@ class Users::ListsController < ApplicationController
   def new; end
 
   ## Should show a specific list
-  def show; end
+  def show
+    binding.pry
+  end
 
   ## NO VIEW
   def edit; end
