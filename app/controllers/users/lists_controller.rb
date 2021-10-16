15ca184ef5327106a049f0334667ec7e5e9c1d56
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-10-16 13:30:22 -0600

    adding in logic and helpers

--- app/controllers/users/lists_controller.rb
+++ app/controllers/users/lists_controller.rb
@@ -1,9 +1,10 @@
 class Users::ListsController < ApplicationController
+  helper_method :set_lists, :set_list
   
   ## Should redirect to show with a random list chosen
   def index
     @list = @user.lists.first
-    
+
     if @list.nil?
       redirect_to new_user_list_path, notice: 'Start here by makeing your first list!'
     else
@@ -13,14 +14,24 @@ class Users::ListsController < ApplicationController
   end
 
   ## NO VIEW creates a new list
-  def create; end
+  def create
+    @list = @user.lists.create(list_params)
+    
+    if @list.save
+      redirect_to user_list_path(id: @list.id), notice: "List was successfully created! Time to add some items"
+    else
+      redirect_to user_lists_path, notice: "Sorry! We can't make heads or tails out of that list right now, please try again in a moment."
+    end
+
+  end
 
   ## Form for new list
   def new; end
 
   ## Should show a specific list
   def show
-    binding.pry
+    @lists = set_lists
+    @list = set_list
   end
 
   ## NO VIEW
@@ -31,4 +42,23 @@ class Users::ListsController < ApplicationController
 
   ##NO VIEW
   def destroy; end
+
+  private
+
+    def list_params
+      params.require(:list).permit(:name, :description)
+    end
+
+    def set_lists
+      @lists = @user.lists.all
+    end
+
+    def set_list
+      if params[:id].nil?
+        @list = @user.lists.first
+      else
+        @list = List.find(params[:id])
+      end
+    end
+
 end
