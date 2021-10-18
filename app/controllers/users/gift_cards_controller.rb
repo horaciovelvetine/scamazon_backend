625241c2c7ce456a4708e0719d08052d05769313
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-10-18 13:48:12 -0600

    init in logic from v1

--- app/controllers/users/gift_cards_controller.rb
+++ app/controllers/users/gift_cards_controller.rb
@@ -1,7 +1,23 @@
 class Users::GiftCardsController < ApplicationController
 
-  def create; end
+  def create 
+    binding.pry
+    @gift_card = @user.gift_cards.create(gift_card_params)
+    User.add_gift_card(@user, @gift_card)
+    
+    if @gift_card.save && @user.save
+      redirect_to gift_card_manger_path, notice: "Card amount successfully added to your account."
+    else
+      redirect_to gift_card_manger_path, notice: "That didn't work. Please try again"
+    end
+  end
 
   def show; end
+
+  private
+
+  def gift_card_params
+    params.require(:gift_card).permit(:redemption_code, :amount, :notes)
+  end
 
 end
