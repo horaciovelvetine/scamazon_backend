9d33b028bf781bf6e620c3477a482113c8083da8
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-10-18 17:21:46 -0600

    begin building search formatter

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,15 @@
 module SearchHelper
+  @@START HERE YOU GOOF 
+  def category_formatter
+    @results = get_categories
+
+    @double = { options => []}
+    @results.each do |category|
+      @double.options << [category, category]
+      return @
+    end
+
+
+  end
+
 end
