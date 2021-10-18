a2bcf03ad8bc5ebb310144a6aa5a93b9f1f1a9cb
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-10-18 17:21:26 -0600

    init in logic for search v2

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,30 @@
-class SearchController < ApplicationController
+class SearchesController < ApplicationController
+
+  def create
+    
+
+    @s = Search.new(search_params)
+    binding.pry
+    @results = @s.get_results
+
+    if @results.length > 0
+      @search.save
+      @results.each do |item|
+        @search.items << item
+      end
+      redirect_to search_path(@search)
+    else
+      redirect_to root_path, notice: "No results matching your search, please try again and pay special attention to your spelling."
+    end
+
+
+  end
+
+
+  private 
+
+  def search_params
+    params.require(:search).permit(:category, :query)
+  end
+
 end
