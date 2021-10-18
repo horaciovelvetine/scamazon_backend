7bfca4878e828246ff49953ca9297eee5d7d341e
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-10-18 17:22:33 -0600

    add cat join, begin logic results

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,3 +1,20 @@
 class Search < ApplicationRecord
   has_and_belongs_to_many :items
+  has_and_belongs_to_many :categories
+
+
+  def get_results(search)
+    binding.pry
+    if search.category == "All"
+      @results = Item.search_by_all(search.query)
+      binding.pry
+    else 
+      @results = search.category.find_by_name(search.category).items.search_by_all(search.query)
+      binding.pry
+    end
+    return @results
+    binding.pry
+  end
+
+
 end
