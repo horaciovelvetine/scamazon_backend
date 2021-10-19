6c98a0e51777bf05a69a02f9f83530ed78ca0c68
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-10-19 13:24:05 -0600

    finish v1 search build and sets

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,30 +1,40 @@
 class SearchesController < ApplicationController
+  before_action :set_search, only: [:show]
 
   def create
     
-
-    @s = Search.new(search_params)
-    binding.pry
-    @results = @s.get_results
-
-    if @results.length > 0
-      @search.save
-      @results.each do |item|
-        @search.items << item
-      end
-      redirect_to search_path(@search)
+    @search = Search.new
+    @search.set_query(@search, params[:search][:query])
+    @search.set_categories(@search, params[:search][:category])
+    
+    if !@user.nil?
+      @search.set_user(@search, @user)
+    end
+    
+    @search.get_results(@search)
+    
+    if @search.items.length > 0
+      redirect_to results_path(id: @search.id)
     else
       redirect_to root_path, notice: "No results matching your search, please try again and pay special attention to your spelling."
     end
 
-
   end
+  
+  def show
+    binding.pry
+  end
+
 
 
   private 
 
   def search_params
-    params.require(:search).permit(:category, :query)
+    params.require(:search).permit(:query, :category)
+  end
+
+  def set_search
+    @search = Search.find(params[:id])
   end
 
 end
