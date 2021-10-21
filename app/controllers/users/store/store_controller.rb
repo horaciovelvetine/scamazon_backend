e1b63ea7c45d684948e18d591672ec5b16d84b1a
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-10-21 10:49:33 -0600

    add b4 act and params def init

--- app/controllers/users/store/store_controller.rb
+++ app/controllers/users/store/store_controller.rb
@@ -1,4 +1,5 @@
 class Users::Store::StoreController < ApplicationController
+before_action :get_user_store
 
   def create; end
 
@@ -13,4 +14,14 @@ class Users::Store::StoreController < ApplicationController
   def update; end
 
   def destroy; end
+
+  private
+    def get_user_store
+      @store = User.store
+    end
+
+
+    def store_params
+      params.require(:store).permit(:name, :email, :address, :store_description, :logo, :bank_account, :user_id)
+    end
 end
