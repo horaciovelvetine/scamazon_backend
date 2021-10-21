f12b5097271cd91dbeee137be3c7fe17614f1b00
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-10-21 13:13:24 -0600

    correcting syntax for through

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,5 +3,5 @@ class Store < ApplicationRecord
   has_one :inventory
   has_many :items, through: :inventory
   has_many :orders, through: :items
-  has_many :users, through: :orders, class_name: "Customers"
+  has_many :customers, through: :orders, source: :user
 end
