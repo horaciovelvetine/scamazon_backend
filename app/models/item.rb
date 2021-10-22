7b2aae66465fefd04c946a3b877d42a323030c61
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-10-21 21:47:00 -0600

    adds items reviewers

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   belongs_to :inventory
   
   has_many :reviews
+  has_many :reviewers, through: :reviews, source: :user
 
   has_and_belongs_to_many :categories
   has_and_belongs_to_many :searches
