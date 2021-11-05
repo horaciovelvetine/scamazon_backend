967f6f9cdbbbb35195f77ed56cc508d58ddc9334
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-05 12:56:16 -0600

    init back in searches resource and migrate

---
+++ app/models/search.rb
@@ -0,0 +1,4 @@
+class Search < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,146 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_11_05_185526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "accounts", force: :cascade do |t|
+    t.string "type"
+    t.string "number"
+    t.datetime "expires"
+    t.string "name_on"
+    t.integer "code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["number"], name: "index_accounts_on_number", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_categories_on_name"
+  end
+
+  create_table "gift_cards", force: :cascade do |t|
+    t.string "redeem_code"
+    t.string "notes"
+    t.decimal "amount", precision: 7, scale: 2
+    t.bigint "wallet_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["redeem_code"], name: "index_gift_cards_on_redeem_code"
+    t.index ["wallet_id"], name: "index_gift_cards_on_wallet_id"
+  end
+
+  create_table "inventories", force: :cascade do |t|
+    t.string "description"
+    t.bigint "store_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["store_id"], name: "index_inventories_on_store_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.boolean "private"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.decimal "sub_total", precision: 10, scale: 2
+    t.decimal "grand_total", precision: 10, scale: 2
+    t.decimal "shipping", precision: 6, scale: 2
+    t.datetime "shipped_on"
+    t.boolean "slime"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.string "query"
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_searches_on_category_id"
+    t.index ["user_id"], name: "index_searches_on_user_id"
+  end
+
+  create_table "shopping_carts", force: :cascade do |t|
+    t.decimal "sub_total", precision: 10, scale: 2
+    t.integer "num_items"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_shopping_carts_on_user_id"
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.string "name"
+    t.string "tracking_info"
+    t.bigint "order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_statuses_on_name"
+    t.index ["order_id"], name: "index_statuses_on_order_id"
+  end
+
+  create_table "stores", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "description"
+    t.string "logo"
+    t.string "industry"
+    t.string "mission_statement"
+    t.string "ein"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_stores_on_name"
+    t.index ["user_id"], name: "index_stores_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "full_name"
+    t.boolean "slime", default: false
+    t.string "phone"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "wallets", force: :cascade do |t|
+    t.string "name"
+    t.decimal "balance", precision: 10, scale: 2
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_wallets_on_user_id"
+  end
+
+  add_foreign_key "gift_cards", "wallets"
+  add_foreign_key "inventories", "stores"
+  add_foreign_key "lists", "users"
+  add_foreign_key "orders", "users"
+  add_foreign_key "searches", "categories"
+  add_foreign_key "searches", "users"
+  add_foreign_key "shopping_carts", "users"
+  add_foreign_key "statuses", "orders"
+  add_foreign_key "stores", "users"
+  add_foreign_key "wallets", "users"
 end
