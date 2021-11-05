01b2112b3c8c50b37dd49ed4802d45c7404a2a58
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-05 15:05:50 -0600

    move CUD actions to nested controller

--- app/controllers/users/store_controller.rb
+++ app/controllers/users/store_controller.rb
@@ -33,3 +33,56 @@ before_action :get_user_store, only: [:show, :edit, :update, :destroy]
       params.require(:store).permit(:name, :email, :address, :description, :logo, :industry, :mission_statement, :ein, :user_id)
     end
 end
+
+## for later ref
+
+  # # GET /stores/new
+  # def new
+  #   @store = Store.new
+  # end
+
+  # # GET /stores/1/edit
+  # def edit
+  # end
+
+  # # POST /stores or /stores.json
+  # def create
+  #   @store = Store.new(store_params)
+
+  #   respond_to do |format|
+  #     if @store.save
+  #       format.html { redirect_to @store, notice: "Store was successfully created." }
+  #       format.json { render :show, status: :created, location: @store }
+  #     else
+  #       format.html { render :new, status: :unprocessable_entity }
+  #       format.json { render json: @store.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
+  # # PATCH/PUT /stores/1 or /stores/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @store.update(store_params)
+  #       format.html { redirect_to @store, notice: "Store was successfully updated." }
+  #       format.json { render :show, status: :ok, location: @store }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @store.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
+  # # DELETE /stores/1 or /stores/1.json
+  # def destroy
+  #   @store.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to stores_url, notice: "Store was successfully destroyed." }
+  #     format.json { head :no_content }
+  #   end
+  # end
+  
+  # # Only allow a list of trusted parameters through.
+  # def store_params
+  #   params.require(:store).permit(:name, :email, :description, :logo, :industry, :mission_statement, :ein, :user)
+  # end
