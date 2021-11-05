46865cfa9c12c424715546cf11e9134de10aa358
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-05 15:26:42 -0600

    add in associations, depend destroys, validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  #Associations:::
+  has_one :store, optional: true
+  has_one :shopping_cart
+  has_one :wallet, dependent: :destroy
+
+  has_many :reviews, , dependent: :destroy
+  has_many :reviewed_items, through: :reviews, source: :item
+  has_many :orders
+  has_many :ordered_items, through: :orders, source: :items
+  has_many :lists, dependent: :destroy
+
+  validates_uniqueness_of :email
+  validates_presence_of :full_name, :phone, :email
+
+
 end
