eade574fbb3c5536f8997c4a6a6614652f81a642
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-05 14:44:16 -0600

    init in users orders actions

--- app/controllers/users/orders_controller.rb
+++ app/controllers/users/orders_controller.rb
@@ -1,13 +1,12 @@
 class Users::OrdersController < ApplicationController
-  prepend_view_path 'app/views/order'
 
   ## Should show all the users orders
   def index; end
 
-  ## No view - should be action after user clicks order
+  ## No view - should be action after user clicks "submit" order
   def create; end
 
-  ## Should be the final confirmation screen before hitting order
+  ## Should be the final confirmation screen before hitting order, order form should be prefilled and provide changeable details
   def new; end
 
   ## Should show completed orders singly
