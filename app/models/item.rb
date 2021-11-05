f1a74fc5b9ea131ccafaad0675b03365b5ee813a
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-05 13:32:08 -0600

    update associations, incl PGsearch and validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,22 @@
 class Item < ApplicationRecord
+  #Associated Models 
   belongs_to :inventory
   belongs_to :category
-  belongs_to :order
-  belongs_to :shopping_cart
+  belongs_to :order, optional: true
+  belongs_to :shopping_cart, optional: true
+  has_one :store, through: :inventory
+
+  has_many :reviews
+  has_many :reviewers, through: :reviews, source: :user
+
+  has_and_belongs_to_many :lists
+  has_and_belongs_to_many :searches
+
+  #Validations
+  validates_format_of :sku, :with => /[0-9]{10}/
+  validates_uniqueness_of :sku
+
+  # Make items searchable through PGSearch inherited PGSearch model from PG Search gem
+  include PgSearch::Model
+  pg_search_scope :search_by_all, against: [:name, :description, :manufacturer, :model, :material]
 end
