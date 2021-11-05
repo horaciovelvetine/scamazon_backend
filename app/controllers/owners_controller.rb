43d9dc66b94789534d11832c56de2a7988bf3e79
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-05 14:33:18 -0600

    outlines intended use for new controller

---
+++ app/controllers/owners_controller.rb
@@ -0,0 +1,6 @@
+class OwnersController < ApplicationController
+
+  # Should provide methods which verify wether or not that person is the owner of whatever content they are accessing and redirect them to the appropriate nested or not controller. 
+
+
+end
