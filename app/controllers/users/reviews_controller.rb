1b6767044c50681e467d87faa14f8ba10651e745
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-05 15:00:16 -0600

    adds pry and actions comments todos

--- app/controllers/users/reviews_controller.rb
+++ app/controllers/users/reviews_controller.rb
@@ -5,4 +5,38 @@ class Users::ReviewsController < ApplicationController
     binding.pry
   end
 
+  def new
+    #gets form for new review by user
+    binding.pry
+  end
+
+  def create
+    #posts form for new review by user
+    binding.pry
+  end
+
+  def show
+    #shows user single review
+    binding.pry
+  end
+
+  def edit
+    #gets edit form by user
+    binding.pry
+  end
+
+  def update
+    #posts edit form by user 
+    binding.pry
+  end
+
+  def destroy
+    #should destroy that users review
+    #should also update items new rating
+    binding.pry
+  end
+  
+
+  
+
 end
