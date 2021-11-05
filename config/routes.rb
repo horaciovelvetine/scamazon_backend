329336368b940af5b62d1f3aedac32e951a60bd1
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-05 12:38:06 -0600

    init in shoppingcart resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :orders
+  resources :shopping_carts
   resources :searches
   resources :inventories
   resources :lists
---
+++ db/migrate/20211105183417_create_shopping_carts.rb
@@ -0,0 +1,11 @@
+class CreateShoppingCarts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :shopping_carts do |t|
+      t.decimal :sub_total, precision: 10, scale: 2
+      t.integer :num_items, null: true
+      t.belongs_to :user, null: true, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
