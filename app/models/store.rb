f4f4f9f25d9d89c096a8ff6adce695dbd97682d9
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-05 20:17:53 -0600

    add missing association

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,6 @@
 class Store < ApplicationRecord
-  has_many: :items, through: :inventory, dependent: :destroy
+  has_many :items, through: :inventory, dependent: :destroy
+  has_one :inventory
+  has_many :orders, through: :items
+  has_many :customers, through: :orders, source: :user
 end
