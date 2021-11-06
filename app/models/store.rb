3a0bbcce57a184b324970d58c4bf6d9110f6c2af
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-05 20:43:03 -0600

    fix belong_to owner

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,5 +3,5 @@ class Store < ApplicationRecord
   has_one :inventory
   has_many :orders, through: :items
   has_many :customers, through: :orders, source: :user
-  belongs_to :owner, source: :user
+  belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
 end
