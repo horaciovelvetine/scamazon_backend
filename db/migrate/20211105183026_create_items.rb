8e73eab981839a5f9dda9d226c1ebd5e4c15f914
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-06 00:07:58 -0600

    set default rating  adds inventory belong

--- db/migrate/20211105183026_create_items.rb
+++ db/migrate/20211105183026_create_items.rb
@@ -5,12 +5,13 @@ class CreateItems < ActiveRecord::Migration[6.1]
       t.string :sku, limit: 10, unique: true
       t.string :description
       t.decimal :price, precision: 10, scale: 2
-      t.decimal :rating, precision: 3, scale: 2
+      t.decimal :rating, precision: 3, scale: 2, default: 0.0
       t.string :manufacturer
       t.string :country_origin
       t.belongs_to :category, null: false, foreign_key: true
       t.belongs_to :order, null: true, foreign_key: true
       t.belongs_to :shopping_cart, null: true, foreign_key: true
+      t.belongs_to :inventory, null: false, foreign_key: true
 
       t.timestamps
     end
