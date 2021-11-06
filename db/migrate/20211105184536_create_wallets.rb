294dc57ed2fd2785b50e12a8f1c7353b4a598744
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-06 00:08:24 -0600

    sets default balance to 0

--- db/migrate/20211105184536_create_wallets.rb
+++ db/migrate/20211105184536_create_wallets.rb
@@ -2,7 +2,7 @@ class CreateWallets < ActiveRecord::Migration[6.1]
   def change
     create_table :wallets do |t|
       t.string :name, default: ''
-      t.decimal :balance, precision: 10, scale: 2
+      t.decimal :balance, precision: 10, scale: 2, default: 0.0
       t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
