190aa6061846d74e12ab6c3e4b918a7da8361427
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-06 00:09:11 -0600

    search doesnt have to belong to user

--- db/migrate/20211105185526_create_searches.rb
+++ db/migrate/20211105185526_create_searches.rb
@@ -2,7 +2,7 @@ class CreateSearches < ActiveRecord::Migration[6.1]
   def change
     create_table :searches do |t|
       t.string :query, null: false
-      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :user, null: true, foreign_key: true
       t.belongs_to :category, null: false, foreign_key: true
 
       t.timestamps
