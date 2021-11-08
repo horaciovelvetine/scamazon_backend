7d292157ffcb3ec22ab3160ca8c3ae2edbfe2579
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-08 14:24:30 -0700

    add in devise/seed routes/logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+  
+
+  def seed; end
+
+  def populate
+    users = params[:seed][:users].to_i
+    items = params[:seed][:itmes].to_i
+    Seed.initiate_seeding(users, items)
+    redirect_to "/", notice: "Seeded Users & Items successfully"
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up)
+    devise_parameter_sanitizer.permit(:account_update)
+  end
+
+  def seed_params
+    params.require(:seed).permit(:users, :items)
+  end
+
+
 end
