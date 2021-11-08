a8f421fb07343e43512e8700cf2d238a7a2d8ebb
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-08 15:09:27 -0700

    uncomment all tested, change syntaxes/calls

--- app/models/seed.rb
+++ app/models/seed.rb
@@ -11,19 +11,12 @@ class Seed < ApplicationRecord
 
   def self.populate_loop(y)
     @user = create_user
-    @list1 = create_list(@user)
-    @list2 = create_list(@user)
-
-    binding.pry
+    3.times { create_list(@user)}
     
-    @store = create_store(@user)
-    y.times {create_item(@store.inventory)}
+    create_store_with_inventory(@user)
+    y.times {create_item(@user.store.inventory)}
 
     binding.pry
-
-
-
-
 
   end
 
@@ -31,48 +24,42 @@ class Seed < ApplicationRecord
 
 
   def self.create_user
-    binding.pry
-    # user = User.create(full_name: Faker::Name.name, email: Faker::Internet.unique.email , phone: Faker::PhoneNumber.cell_phone, password: 'default')
-    # wallet = user.wallet.create(name: "#{user.full_name}'s Wallet")
-    # address = user.address.create(street: Faker::Address.street_address, unit: Faker::Address.secondary_address, city: Faker::Address.city, state: Faker::Address.state, zip: Faker::Address.zip)
-    # account = wallet.accounts.create(type: Faker::Stripe.valid_token, number: Faker::Stripe.valid_card, expires: "#{Faker::Stripe.month}/#{Faker::Stripe.year}", security_code: Faker::Stripe.ccv, address: address)
+    user = User.create(full_name: Faker::Name.name, email: Faker::Internet.unique.email , phone: Faker::PhoneNumber.cell_phone, password: 'default')
     
+    user.create_wallet(name: "#{user.full_name}'s Wallet")
     
+    address = user.addresses.create(street: Faker::Address.street_address, unit: Faker::Address.secondary_address, city: Faker::Address.city, state: Faker::Address.state, zip: Faker::Address.zip)
     
-    sc = user.shopping_cart.create ###################################################################################
-    ####<------- This should add all of the default values to the cart and not need attributes so start there
-    
-    
-    # return user
+    user.wallet.accounts.create(issued_by: Faker::Stripe.valid_token, number: Faker::Stripe.valid_card, expires: "#{Faker::Stripe.month}/#{Faker::Stripe.year}", ccv: Faker::Stripe.ccv, address: address, name_on: user.full_name)
+    user.create_shopping_cart
+    return user
   end
-
-  def self.create_store(user)
-    binding.pry
-    # store = user.store.create(name: Faker::Company.name,, email: Faker::Internet.unique.email, description: Faker::Hipster.paragraph(sentence_count: 3), logo: Faker::Company.logo, industry:Faker::Company.industry, mission_statement: Faker::Company.catch_phrase, ein: Faker::Company.unique.ein)
-    # inventory = store.inventory.create
-    # return store
-  end
-
+  
   def self.create_list(user)
+    list = user.lists.create(name: Faker::Commerce.department, description: Faker::Hipster.paragraph(sentence_count: 1))
+  end
+
+
+  def self.create_store_with_inventory(user)
+    store = user.create_store(name: Faker::Company.name, email: Faker::Internet.unique.email, description: Faker::Hipster.paragraph(sentence_count: 3), logo: Faker::Company.logo, industry:Faker::Company.industry, mission_statement: Faker::Company.catch_phrase, ein: Faker::Company.unique.ein)
+    store.create_inventory
     binding.pry
-    # list = user.lists.create(name: Faker::Commerce.department, description: Faker::Hipster.paragraph(sentence_count: 1))
-    # return list
   end
 
   def self.create_item(inventory)
-    binding.pry
     # rand_category = Categories.all.order("RANDOM()").first
     
     # item = inventory.items.create(name: Faker::Commerce.unique.product_name, sku: Faker::Number.unique.number(digits: 10), description: Faker::Hipster.paragraph(sentence_count: 12), price: Faker::Commerce.price, manufacturer: Faker::Commerce.vendor, country_origin: Faker::Address.country, category: rand_category)
-
+    
     # rand(1..5).times { create_styles(item)}
+    binding.pry
   end
 
   def self.create_styles(item)
-    binding.pry
     # col = Faker::Commerce.color
     # mat = Faker::Commerce.material
     # style = item.styles.create(name: "#{col} #{mat}", model: Faker::Company.duns_number, color: col, material: mat, quantity: rand(1..50), weight: Faker::Measurement.weight, dimensions: "#{Faker::Measurement.length} x #{Faker::Measurement.height} x #{Faker::Measurement.length")
+    binding.pry
   end
 
 
