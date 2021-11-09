e0deaea1f17a5890a5aa4bdb218970ab94c32fcd
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-09 13:30:34 -0700

    stub out order methods for update process

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,50 @@ class Order < ApplicationRecord
   belongs_to :status
   has_many :items
   has_many :stores, through: :items
+
+  def update_order_information
+
+  end
+
+  def calculate_sub_total
+    #calculates the sub totals based on all item prices
+    # self.items.each do |item|
+    #   self.sub_total += item.price
+    # end
+    binding.pry
+  end
+
+  def add_items(items)
+    #iterates over the returns array, and adds items to the order
+    items.each do |item|
+      self << item
+    end
+    binding.pry
+  end
+
+  def update_shipping_info(info)
+    #should take in info form store form, and update the order
+    # self.shipping_info = info[:info]
+    # self.shipped_on = info[:shipped_on]
+    # self.shipping_cost = shipping_calculator(self)
+    binding.pry
+  end
+
+  def update_tracking_info(tracking)
+    #Should take in tracking info to update the ordera
+    self.tracking_info = tracking
+    binding.pry
+  end
+
+  def shipping_calculator
+    #Eventually should take the orders address and calculate the cost from A to B. In demo will provide random price. 
+    if self.slime?
+      return 0.00
+    else
+      rand(5.99..28.99)
+    end
+    binding.pry
+  end
+
+
 end
