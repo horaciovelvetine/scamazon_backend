5d857472cece0776b90638c5029fe0a75f24d8ec
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-10 14:19:30 -0700

    taught to update num_items based on styles assoc

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -1,4 +1,16 @@
 class ShoppingCart < ApplicationRecord
   belongs_to :user, optional: true
+  has_many :styles
+
+
+  #Iterates over carts styles, checks the quantity in the cart, and adds that to the attribute
+  def update_number_of_items
+    self.num_items = 0 
+    self.styles.each do |style|
+      self.num_items += .style.quantity
+    end
+  end
+
+
 end
 
