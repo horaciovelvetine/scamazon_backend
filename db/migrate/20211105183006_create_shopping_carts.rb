d7a91f15070217b5288b72f414f71c9246138271
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-10 14:16:54 -0700

    remove excess null statement

--- db/migrate/20211105183006_create_shopping_carts.rb
+++ db/migrate/20211105183006_create_shopping_carts.rb
@@ -2,7 +2,7 @@ class CreateShoppingCarts < ActiveRecord::Migration[6.1]
   def change
     create_table :shopping_carts do |t|
       t.decimal :sub_total, precision: 10, scale: 2, default: 0.0
-      t.integer :num_items, null: true, default: 0
+      t.integer :num_items, default: 0
       t.belongs_to :user, null: true, foreign_key: true
 
       t.timestamps
