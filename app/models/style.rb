68eaf754cf55904fd6ba335457d386a1a67789fa
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-10 15:15:37 -0700

    add sku validation for length presence

--- app/models/style.rb
+++ app/models/style.rb
@@ -3,4 +3,8 @@ class Style < ApplicationRecord
   belongs_to :order, optional: true
   belongs_to :shopping_cart, optional: true
   belongs_to :return, optional: true
+
+    #Validations
+  validates_format_of :sku, :with => /[0-9]{10}/
+  validates_uniqueness_of :sku
 end
