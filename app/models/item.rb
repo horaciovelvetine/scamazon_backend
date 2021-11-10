a84227296b5d0743855ec3da4591f8c0f40be0c3
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-10 15:26:04 -0700

    fix inherited belongs_to to work /destroy

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,11 @@
 class Item < ApplicationRecord
   #Associated Models 
   belongs_to :inventory
+  delegate :store, :to => :inventory
   belongs_to :category
   belongs_to :order, optional: true
   belongs_to :shopping_cart, optional: true
-  has_one :store, through: :inventory
+  
 
   has_many :styles, dependent: :destroy
   has_many :quesitons, dependent: :destroy
@@ -12,7 +13,6 @@ class Item < ApplicationRecord
   has_many :viewers, through: :viewed_items, source: :user
   has_many :reviews, dependent: :destroy
   has_many :reviewers, through: :reviews, source: :user
-  has_many :returns
 
   has_and_belongs_to_many :lists
   has_and_belongs_to_many :tags
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,6 +1,6 @@
 class Store < ApplicationRecord
-  has_many :items, through: :inventory, dependent: :destroy
-  has_one :inventory
+  has_one :inventory, dependent: :destroy
+  has_many :items, through: :inventory
   has_many :orders, through: :items
   has_many :customers, through: :orders, source: :user
   belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
