42ea68a7208ab6ddbc8d1feae8974d56b317ff5c
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-10 15:14:48 -0700

    init list adding items, order calls correctly

--- app/models/seed.rb
+++ app/models/seed.rb
@@ -2,20 +2,20 @@ class Seed < ApplicationRecord
 
 
   def self.initiate_seeding(x, y)
+    x.times { populate_loop(y) }
+  end
+  
+  
+  def self.populate_loop(y)
     if y == 0 
       y = rand(1..20)
     end
-    x.times { populate_loop(y) }
-  end
-
-
-  def self.populate_loop(y)
     @user = create_user
-    3.times { create_list(@user)}
     
     create_store_with_inventory(@user)
     y.times {create_item(@user.store.inventory)}
     
+    2.times { create_list(@user)}
     3.times{ create_orders_for_user(@user)}
   end
 
@@ -34,34 +34,39 @@ class Seed < ApplicationRecord
     return user
   end
   
-  def self.create_list(user)
-    list = user.lists.create(name: Faker::Commerce.department, description: Faker::Hipster.paragraph(sentence_count: 1))
-  end
-
-
+  
+  
   def self.create_store_with_inventory(user)
     store = user.create_store(name: Faker::Company.name, email: Faker::Internet.unique.email, description: Faker::Hipster.paragraph(sentence_count: 3), logo: Faker::Company.logo, industry:Faker::Company.industry, mission_statement: Faker::Company.catch_phrase, ein: Faker::Company.unique.ein)
     store.create_inventory
   end
-
+  
   def self.create_item(inventory)
     item = inventory.items.create(name: Faker::Commerce.unique.product_name,  description: Faker::Hipster.paragraph(sentence_count: 12),  category: Category.all.order("RANDOM()").first)
     
     rand(1..5).times {create_styles(item)}
   end
-
+  
   def self.create_styles(item)
     col = Faker::Commerce.color
     mat = Faker::Commerce.material
     style = item.styles.create(name: "#{col} #{mat}", model: Faker::Company.duns_number, sku: Faker::Number.unique.number(digits: 10), price: Faker::Commerce.price,color: col, material: mat, quantity: rand(1..50), weight: Faker::Measurement.weight, manufacturer: Faker::Commerce.vendor, country_origin: Faker::Address.country, dimensions: "#{Faker::Measurement.length} x #{Faker::Measurement.height} x #{Faker::Measurement.length}")
   end
 
+  def self.create_list(user)
+    list = user.lists.create(name: Faker::Commerce.department, description: Faker::Hipster.paragraph(sentence_count: 1))
+    @items = Item.all.order("RANDOM()").take(rand(5..10))
+    list.add_items(@items)
+  end
+  
+
+  ## Creates dummy order with: Items, Styles, Random Attr info, updates all styles to correct quantities (removes store quantity, and adds to creates duplicate style belonging to order instead with quantity ordered). 
   def self.create_orders_for_user(user)
     
     #grabs sample item/style info from database to create Order
-    items = Item.all.order("RANDOM()").take(3)
-    styles = items.map { |i| i.styles.order("RANDOM()").first }
-    quantities = Array.new(items.length) { rand(1..3) }
+    @items = Item.all.order("RANDOM()").take(rand(2..5))
+    styles = @items.map { |i| i.styles.order("RANDOM()").first }
+    quantities = Array.new(@items.length) { rand(1..3) }
     
 
     #grabs and creates information to populate new order
@@ -74,7 +79,7 @@ class Seed < ApplicationRecord
     
 
     #Call all order update methods to assign info to order
-    order.add_items(items)
+    order.add_items(@items)
     order.add_update_and_duplicate_styles(styles, quantities)
     order.update_shipping_info(ship_info)
     order.tracking_info = tracking
@@ -83,12 +88,7 @@ class Seed < ApplicationRecord
     order.calculate_grand_total
     order.save
 
-    binding.pry
-
-
   end
-
-
 
 end
 
