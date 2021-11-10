e368e0a82ab58c99614d0b7d1ef347d0e3e374ee
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-10 14:17:36 -0700

    slim item down 2 min. attr to make search faster

--- db/migrate/20211105183026_create_items.rb
+++ db/migrate/20211105183026_create_items.rb
@@ -2,11 +2,8 @@ class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
       t.string :name, unique: true
-      t.bigint :sku, unique: true
       t.string :description
       t.decimal :rating, precision: 3, scale: 2, default: 0.0
-      t.string :manufacturer
-      t.string :country_origin
       t.belongs_to :category, null: false, foreign_key: true
       t.belongs_to :order, null: true, foreign_key: true
       t.belongs_to :shopping_cart, null: true, foreign_key: true
@@ -15,7 +12,6 @@ class CreateItems < ActiveRecord::Migration[6.1]
       t.timestamps
     end
     add_index :items, :name
-    add_index :items, :sku
   end
 
 end
