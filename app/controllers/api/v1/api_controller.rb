07847d8acbd46c38c0c226dd9bff51fb4af06058
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-16 14:53:27 -0700

    nest controller under correct modules

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,8 +1,9 @@
-class ApiController < ApplicationController
-
-
-  def demo_cart
-    binding.pry
+module api
+  module v1
+    class ApiController < ApplicationController
+      def demo_cart
+        binding.pry
+      end
+    end
   end
-
 end
