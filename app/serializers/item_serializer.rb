7a834479e43aab2fd2389e555a8631e6360c8a6e
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-16 16:59:59 -0700

    cart --> shopping_cart

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,10 +1,10 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
+  belongs_to :shopping_cart
   attributes :name, :rating
 
   attribute :number_of_reviews do |item|
     item.reviews.length
   end
 
-  belongs_to :cart
 end
