4915599f152862772757fe1b94ee2b7439c90acf
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-16 16:10:56 -0700

    init in range language for later

--- app/serializers/spon_item_serializer.rb
+++ app/serializers/spon_item_serializer.rb
@@ -2,4 +2,15 @@ class SponItemSerializer
   include JSONAPI::Serializer
 
   has_many :items do 
+
+
+  ## doesnt make sense here, saving for alter
+  # attribute :price_range do |item|
+  #   ## Checks the number of styles an item has, if 1 returns that single price as the price range
+  #   if item.styles.length == 1
+  #     item.styles.first.price
+  #   else
+  #     item.styles.map {|style| style.price}.minmax
+  #   end
+  # end
 end
