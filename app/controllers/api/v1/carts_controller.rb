cd597189428cc5735022c3e1cbc345681695c0ae
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-16 16:58:18 -0700

    rename to have data make sense

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -1,7 +1,9 @@
-module api
-  module v1
-    class ApiController < ApplicationController
-      def show_cart
+module API
+  module V1
+    class CartsController < ActionController::Base
+      # skip_before_action :verify_authenticity_token
+      
+      def show
         ## This action will need to return some JSON which should include the details of a random users cart (the seeding provides any number of users w/ orders.)
 
         ## The details that are going to be needed:
@@ -12,9 +14,8 @@ module api
           ##ea style: quantity in stock, qty.
 
           ## JSON wise, I would like to return the shopping_cart with its associated items, which have associated styles
-        cart = ShoppingCart.all.order("RANDOM()").last
-        binding.pry
-        render json: CartSerializer.new(cart).serializable_hash.to_json
+        shopping_cart = ShoppingCart.all.order("RANDOM()").last
+        render json: ShoppingCartSerializer.new(shopping_cart).serializable_hash.to_json
 
 
       end
