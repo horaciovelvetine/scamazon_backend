f4c53fb34a44bc787e137d3bb29da5357ba90d7a
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-16 16:23:59 -0700

    adds pry start api response testing

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -13,7 +13,7 @@ module api
 
           ## JSON wise, I would like to return the shopping_cart with its associated items, which have associated styles
         cart = ShoppingCart.all.order("RANDOM()").last
-
+        binding.pry
         render json: CartSerializer.new(cart).serializable_hash.to_json
 
 
