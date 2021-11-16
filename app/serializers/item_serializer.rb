ff8dbcb8c4f169044ad5f4fced5cf2ceeb6d8a19
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-16 16:10:45 -0700

    removes style, adds b2

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,26 +1,10 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :rating
-  has_many :styles
 
   attribute :number_of_reviews do |item|
     item.reviews.length
   end
 
-
-
-  ## doesnt make sense here, saving for alter
-  # attribute :price_range do |item|
-  #   ## Checks the number of styles an item has, if 1 returns that single price as the price range
-  #   if item.styles.length == 1
-  #     item.styles.first.price
-  #   else
-  #     item.styles.map {|style| style.price}.minmax
-  #   end
-  # end
-
-
-
-
-
+  belongs_to :cart
 end
