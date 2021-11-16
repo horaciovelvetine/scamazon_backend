05cfe419c9f6759bbad8f4e7bb91df1e391292d4
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-16 16:11:36 -0700

    comment out add'l items for now

--- app/serializers/cart_serializer.rb
+++ app/serializers/cart_serializer.rb
@@ -2,7 +2,7 @@ class CartSerializer
   include JSONAPI::Serializer
 
   has_many :items, :styles
-  has_one :related_items, :spon_items
+  # has_one :related_items, :spon_items
 
   attribute :sub_total
   attribute :number_of_items, &:num_items
