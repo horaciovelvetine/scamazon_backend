52e4072eb11be719957cea12040f767a003ab174
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-16 17:00:50 -0700

    del bad name

--- app/serializers/cart_serializer.rb
+++
@@ -1,9 +0,0 @@
-class CartSerializer
-  include JSONAPI::Serializer
-
-  has_many :items, :styles
-  # has_one :related_items, :spon_items
-
-  attribute :sub_total
-  attribute :number_of_items, &:num_items
-end
