f2556dc6705fb93f7075f033ff95559c08a4da45
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-17 00:21:49 -0700

    delete additional un-needed serializers

--- app/serializers/related_item_serializer.rb
+++
@@ -1,5 +0,0 @@
-class RelatedItemSerializer
-  include JSONAPI::Serializer
-
-  
-end
--- app/serializers/style_serializer.rb
+++
@@ -1,7 +0,0 @@
-class StyleSerializer
-  include FastJsonapi::ObjectSerializer
-  belongs_to :shopping_cart
-  attributes :price, :quantity
-
-  
-end
