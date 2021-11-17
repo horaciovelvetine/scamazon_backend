c1d73e3b6af1f1d14bcd4dcd3f8220e2330395c6
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-17 00:22:42 -0700

    fix price issue, + spon/rel items arr, +comments

--- app/serializers/shopping_cart_serializer.rb
+++ app/serializers/shopping_cart_serializer.rb
@@ -6,10 +6,16 @@ class ShoppingCartSerializer
 
   attribute :sub_total
 
+
+
+  ## Array of specially formatted items for providing info needed to display a combo of style/item info
   attribute :cart_items do |cart|
     cart_items = cart.items.each_with_index.map { |item, i| {id: item.id, name: "#{item.name}", rating: item.rating, number_of_reviews: item.reviews.length, price: cart.styles[i].price.to_i, quantity: cart.styles[i].quantity}} 
   end
 
+
+
+  ## Array of items related to current items in cart by
   attribute :related_items do |cart|
     item_bucket = []
 
@@ -19,18 +25,30 @@ class ShoppingCartSerializer
       end
     end
     
-    related_items = item_bucket.map {|i| {id: i.id, name: "#{i.name}", category: "#{i.category.name}", rating: i.rating, number_of_reviews: i.reviews.length, price: i.styles.map{|s| s.price.to_i}.minmax} }
+    related_items = item_bucket.map { |i| {id: i.id, name: "#{i.name}", category: "#{i.category.name}", rating: i.rating, number_of_reviews: i.reviews.length, price: price_range(i)} }
     
 
   end
 
+  ## Array of random items w/ relevant info
   attribute :sponsored_items do |cart|
-    sponsored_items = Item.all.order("RANDOM()").take(6).map {|i| {id: i.id, name: "#{i.name}", rating: i.rating, number_of_review: i.reviews.length, price: i.styles.map{|s| s.price.to_i}.minmax}}
+    sponsored_items = Item.all.order("RANDOM()").take(6).map { |i| {id: i.id, name: "#{i.name}", rating: i.rating, number_of_review: i.reviews.length, price: price_range(i)} }
+  end
+  
+  ## Checks if an items/styles to find and return either a single or range price for an item display
+  def self.price_range(item)
+  
+    if item.styles.length == 1
+      item.styles.first.price
+    else
+      item.styles.map {|style| style.price}.minmax.uniq
+    end
   end
 
 
-
-
+  
+  
+  
   attributes :items, :styles
 
 
