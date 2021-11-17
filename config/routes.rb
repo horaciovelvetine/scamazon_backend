5a7ea550e2308c8bc364b94683e2c923d32eb741
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-16 17:00:45 -0700

    rename to valid inside rails

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       ## define routes here for /api/v1/...
-      get '/show_cart', to: 'api_controller#show_cart'
+      get '/show_cart', to: 'carts#show'
     end
   end
 
