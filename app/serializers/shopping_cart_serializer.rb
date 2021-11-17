24cf8b5e0b28f6d02f8df88552b1b4f660a6a24f
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-16 23:20:13 -0700

    setup cartItem attr, structure return data json

--- app/serializers/shopping_cart_serializer.rb
+++ app/serializers/shopping_cart_serializer.rb
@@ -2,11 +2,16 @@ class ShoppingCartSerializer
   include JSONAPI::Serializer
   set_type :shopping_cart
   set_id :user_id
-
   attribute :number_of_items, &:num_items
-  attributes :sub_total, :items, :styles
+
+  attribute :sub_total
 
   # has_one :related_items, :spon_items
+  attribute :cart_items do |cart|
+    cart_items = cart.items.each_with_index.map { |item, i| {id: item.id, name: "#{item.name}", rating: item.rating, number_of_reviews: item.reviews.length, price: cart.styles[i].price.to_i, quantity: cart.styles[i].quantity}} 
+  end
+
+  attributes :items, :styles
 
 
 end
