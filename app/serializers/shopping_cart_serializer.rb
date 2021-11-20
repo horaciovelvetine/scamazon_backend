0b6aafa48e1e93a102dd0f7aede57d10b73e654e
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-19 17:31:07 -0700

    tweaks JSON response for expected FE

--- app/serializers/shopping_cart_serializer.rb
+++ app/serializers/shopping_cart_serializer.rb
@@ -8,7 +8,7 @@ class ShoppingCartSerializer
 
   ## Array of specially formatted items for providing info needed to display a combo of style/item info
   attribute :cart_items do |cart|
-    cart_items = cart.items.each_with_index.map { |item, i| {id: item.id, name: "#{item.name}", rating: item.rating, number_of_reviews: item.reviews.length, price: cart.styles[i].price.to_i, quantity: cart.styles[i].quantity}} 
+    cart_items = cart.items.each_with_index.map { |item, i| {id: item.id, name: item.name.titleize, rating: item.rating, number_of_reviews: item.reviews.length, price: cart.styles[i].price, quantity: cart.styles[i].quantity, color: cart.styles[i].color.titleize, manufacturer: cart.styles[i].manufacturer.titleize} } 
   end
 
   ## Array of items related to current items in cart by
@@ -21,13 +21,13 @@ class ShoppingCartSerializer
       end
     end
     
-    related_items = item_bucket.map { |i| {id: i.id, name: "#{i.name}", category: "#{i.category.name}", rating: i.rating, number_of_reviews: i.reviews.length, price: price_range(i)} }
+    related_items = item_bucket.map { |i| {id: i.id, name: i.name.titleize, category: i.category.name.titleize, rating: i.rating, number_of_reviews: i.reviews.length, price: price_range(i)} }
   
   end
 
   ## Array of random items w/ relevant info
   attribute :sponsored_items do |cart|
-    sponsored_items = Item.all.order("RANDOM()").take(6).map { |i| {id: i.id, name: "#{i.name}", rating: i.rating, number_of_reviews: i.reviews.length, price: price_range(i)} }
+    sponsored_items = Item.all.order("RANDOM()").take(6).map { |i| {id: i.id, style: i.name.titleize, rating: i.rating, number_of_reviews: i.reviews.length, price: price_range(i)} }
   end
   
   ## Checks if an items/styles to find and return either a single or range price for an item display
