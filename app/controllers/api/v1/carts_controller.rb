aca5d708f9b082bee61af1663a94c4224d137694
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-22 15:32:28 -0700

    remove skip stuff to application init

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -1,8 +1,7 @@
 module API
   module V1
     class CartsController < ActionController::Base
-      # skip_before_action :verify_authenticity_token
-      
+
       def show
         shopping_cart = ShoppingCart.all.order("RANDOM()").last
         render json: ShoppingCartSerializer.new(shopping_cart).serializable_hash.to_json
