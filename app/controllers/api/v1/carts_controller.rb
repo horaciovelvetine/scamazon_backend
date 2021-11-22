4d6be66a91d93bef2a612c58741e4f02da66a069
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-22 13:22:16 -0700

    set up landing spots for testing

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -4,21 +4,19 @@ module API
       # skip_before_action :verify_authenticity_token
       
       def show
-        ## This action will need to return some JSON which should include the details of a random users cart (the seeding provides any number of users w/ orders.)
-
-        ## The details that are going to be needed:
-          ##the cart: sub_tot, number of items(quantity of each style), and items themselves - needs a FE Object as a reflection
-
-          ##ea item: name, price(range??), rating (number of ratings)
-
-          ##ea style: quantity in stock, qty.
-
-          ## JSON wise, I would like to return the shopping_cart with its associated items, which have associated styles
         shopping_cart = ShoppingCart.all.order("RANDOM()").last
         render json: ShoppingCartSerializer.new(shopping_cart).serializable_hash.to_json
-
-
       end
+
+      def update
+        binding.pry
+      end
+
+      def destroy
+        binding.pry
+      end
+
+      
     end
   end
 end
