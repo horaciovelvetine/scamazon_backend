a27ff4e0ee8cf15fa520904fe339b1dbf8bd9c35
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-22 21:21:41 -0700

    updates style quantities on shopping_cart

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -17,12 +17,14 @@ class ShoppingCart < ApplicationRecord
   def update_style_quantities(params)
     update_hash = {}
     params[:cartItems].each do |item|
-      binding.pry
       update_hash.store(item[:id], item[:quantity])
     end
 
-    update_hash.each_pair{|id, q| Style.find(id).quantity = q}
-    binding.pry
+    update_hash.each_pair do |id, q| 
+      @style = Style.find(id)
+      @style.update_attribute(:quantity, q)
+    end
+
   end
 
 
