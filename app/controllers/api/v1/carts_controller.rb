3606ecb257a577ddd93addcf16b437f92b9be67f
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-22 21:21:22 -0700

    add logic for destroy cart and remove item

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -5,19 +5,30 @@ module API
 
 
       def show
-        shopping_cart = ShoppingCart.all.order("RANDOM()").last
-        render json: ShoppingCartSerializer.new(shopping_cart).serializable_hash.to_json
+        cart = ShoppingCart.all.order("RANDOM()").last
+        render json: ShoppingCartSerializer.new(cart).serializable_hash.to_json
       end
 
       def update
         cart = ShoppingCart.find(params[:id])
         cart.update_style_quantities(params)
-
+        render json: {message: 'Cart updated successfully'}
       end
 
       def destroy
+        cart = ShoppingCart.find(params[:id])
+        user = cart.user
         binding.pry
+        # cart.destroy
+        # user.create_cart
       end
+
+      def remove_item
+        style = ShoppingCart.find(params[:id]).styles.find(params[:style_id])
+        style.destroy
+        render json: {message: 'Item Removed Successfully'}
+      end
+
 
       
     end
